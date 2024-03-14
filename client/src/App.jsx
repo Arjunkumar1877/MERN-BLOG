a4a99f1f5d517de7728a1fda39be3007bd922213
Arjun kumar vs <arjun.tech177@gmail.com>
2024-03-15 02:16:38 +0530

    Added sign in functionalities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn'
 import Projects from './pages/Projects'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
     <Route path='/dashbord' element={<Dashboard />} />
     <Route path='/projects' element={<Projects />} />
    </Routes>
+
+   <Footer />
 </BrowserRouter>  )
 }
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Label, TextInput } from 'flowbite-react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e)=>{
+    setFormData({...formData, [e.target.id]: e.target.value.trim()})
+    console.log(e.target.value);
+  };
+
+  console.log(formData);
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage("Please fill out all fields.");
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(formData)
+      })
+
+      const data = await res.json();
+      if(data.success === false){
+       setErrorMessage(data.message)
+      }
+
+      setLoading(false)
+      navigate('/sign-in');
+
+      console.log(data)
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+    
+  }
+
+  console.log(errorMessage)
+
   return (
     <div className='min-h-screen mt-20'>
      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-5">
@@ -17,30 +62,42 @@ function SignUp() {
 
 {/* right side */}
 <div className="flex-1">
-<form className='flex flex-col gap-4'>
+<form className='flex flex-col gap-4' onSubmit={handleSubmit}>
   <div className="">
     <Label value='Your username' />
-    <TextInput type='text' placeholder='Username' id='username' />
+    <TextInput onChange={handleChange} type='text' placeholder='Username' id='username' />
   </div>
 
   <div className="">
     <Label value='Your email' />
-    <TextInput type='text' placeholder='name@company.com' id='email' />
+    <TextInput onChange={handleChange} type='email' placeholder='name@company.com' id='email' />
   </div>
 
   <div className="">
     <Label value='Your password' />
-    <TextInput type='text' placeholder='password' id='password' />
+    <TextInput onChange={handleChange} type='password' placeholder='password' id='password' />
   </div>
 
-  <Button gradientDuoTone='purpleToPink' type='submit'>
-    Sign Up
+  <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+    {loading ? (
+     <>
+      <Spinner  size='sm'/>
+      <span className='pl-3'>Loading...</span>
+     </>
+    ) : "Sign Up"}
     </Button>
 </form>
 <div className="flex gap-2 text-sm mt-5">
   <span>Have an account?</span>
   <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
 </div>
+{
+  errorMessage && (
+    <Alert className='mt-5' color='failure'>
+      {errorMessage}
+    </Alert>
+  )
+}
 </div>
      </div>
     </div>
