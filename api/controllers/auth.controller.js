7a79da4d947e3bec51f04e2847ba3b80e13f9b12
Arjun kumar vs <arjun.tech177@gmail.com>
2024-03-15 03:20:34 +0530

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs'
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import  jwt  from 'jsonwebtoken';
 
 
 export const signup = async(req,res, next)=>{
@@ -27,5 +28,39 @@ export const signup = async(req,res, next)=>{
   } catch (error) {
      next(error)
   }
- }
+}
+
+
+export const signin = async(req,res, next)=>{
+   const { email, password } = req.body;
+
+   if(!email || !password || email === '' || password === ''){
+      next(errorHandler(400, "All fields required"));
+   }
+
+   try {
+      const validUser = await User.findOne({email: email});
+
+      if(!validUser){
+      return next(errorHandler(400, 'User not found !'));
+      }
+
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+      if(!validPassword){
+       return  next(errorHandler(400, 'Invalid credential !'))
+      }
+
+      const token = jwt.sign({
+         id: validUser._id
+      }, process.env.JWT_SECRET)
+
+      const {password: passs, ...restData} = validUser._doc;
+
+      res.status(200).cookie('access_token', token, {httpOnly: true}).json(restData)
+   } catch (error) {
+      next(error)
+   }
+
+}
  
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    unique: true
    
   }
 }, {timestamps: true});
