c8d148a98d48c1826ef6408c96949293199399dc
Arjun kumar vs <arjun.tech177@gmail.com>
2024-03-14 23:17:50 +0530

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
 const app = express();
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('MongoDb connected')
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 
 app.listen(3000, ()=>{
