87545a6b89b3322ad2f4af384e05cecba2ce6582
Arjun kumar vs <arjun.tech177@gmail.com>
2024-03-20 02:50:58 +0530

    Added Dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,84 +2,84 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
+
 
 function Header() {
+    const dispatch = useDispatch();
     const path = useLocation().pathname;
     const { currentUser } = useSelector(state => state.user);
-  return (
-    <Navbar className='border-b-2 '>
- <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold  dark:text-white'>
-    <span className='px-2 py-1 rounded-lg  bg-gradient-to-r from-indigo-500  via-purple-500 to-pink-500 text-white'>Arjun's</span>
-    Blog
- </Link>
+    const { theme } = useSelector(state => state.theme);
+    return (
+        <Navbar className='border-b-2 '>
+            <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold  dark:text-white'>
+                <span className='px-2 py-1 rounded-lg  bg-gradient-to-r from-indigo-500  via-purple-500 to-pink-500 text-white'>Arjun's</span>
+                Blog
+            </Link>
+            <form >
+                <TextInput type='text' placeholder='Search...' rightIcon={AiOutlineSearch} className='hidden lg:inline' />
+            </form>
+            <Button className='w-12 h-10 lg:hidden' pill color='gray'>
+                <AiOutlineSearch />
+            </Button>
+            <div className="flex gap-2 md:order-2">
+                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill  onClick={()=> dispatch(toggleTheme())}>
+                   {
+                    theme === 'dark' ?  <FaMoon  /> : <FaSun />
+                   }
+                </Button>
+                {
+                    currentUser ? (
+                        <Dropdown arrowIcon={false}
+                            inline
+                            label={
+                                <Avatar alt='user' img={currentUser.profilePicture} rounded />
+                            }
+                        >
+                            <Dropdown.Header>
+                                <span className='block text-sm'>@{currentUser.username}</span>
+                                <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                            </Dropdown.Header>
+                            <Link to={'/dashboard?tab=profile'}>
+                                <Dropdown.Item>Profile</Dropdown.Item>
+                            </Link>
+                            <Dropdown.Divider />
+                            <Link to={"/sign-out"}>
+                                <Dropdown.Item>Sign out</Dropdown.Item>
+                            </Link>
+                        </Dropdown>
+                    ) : (
+                        <Link to='/sign-in'>
+                            <Button gradientDuoTone='purpleToBlue' outline>
+                                Sign in
+                            </Button>
+                        </Link>
+                    )
+                }
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse>
+                <Navbar.Link active={path === '/'} as={'div'}>
+                    <Link to='/'>Home</Link>
+                </Navbar.Link>
 
-<form >
-<TextInput type='text' placeholder='Search...'  rightIcon={AiOutlineSearch} className='hidden lg:inline' />
-</form>
-<Button className='w-12 h-10 lg:hidden' pill  color='gray'>
-    <AiOutlineSearch />
-</Button>
-<div className="flex gap-2 md:order-2">
-    <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-       <FaMoon />
-    </Button>
+                <Navbar.Link active={path === '/about'} as={'div'}>
+                    <Link to='/about'>
+                        About
+                    </Link>
+                </Navbar.Link>
 
-{
-    currentUser ? (
-        <Dropdown arrowIcon={false}  
-          inline
-          label={
-            <Avatar alt='user' img={currentUser.profilePicture} rounded />
-          }
-        >
+                <Navbar.Link active={path === '/projects'} as={'div'}>
+                    <Link to='/projects'>
+                        Projects
+                    </Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
 
-<Dropdown.Header>
-    <span className='block text-sm'>@{currentUser.username}</span>
-    <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
-</Dropdown.Header>
- <Link  to={'/dashboard?tab=profile'}>
-   <Dropdown.Item>Profile</Dropdown.Item>
-</Link>
-<Dropdown.Divider />
-<Link to={"/sign-out"}> 
-<Dropdown.Item>Sign out</Dropdown.Item>
-</Link>
-
-        </Dropdown>
-    ) : (
-        <Link to='/sign-in'>
-    <Button  gradientDuoTone='purpleToBlue' outline>
-      Sign in
-    </Button>
-</Link>
+        </Navbar>
     )
-}
-
-<Navbar.Toggle/>
-</div>
-
-<Navbar.Collapse>
-    <Navbar.Link active={path === '/'}  as={'div'}>
-    <Link to='/'>Home</Link>
-    </Navbar.Link>
-
-    <Navbar.Link  active={path === '/about'} as={'div'}>
-        <Link to='/about'>
-            About
-        </Link>
-    </Navbar.Link>
-
-    <Navbar.Link active={path === '/projects'} as={'div'}>
-        <Link to='/projects'>
-            Projects
-        </Link>
-    </Navbar.Link>
-</Navbar.Collapse>
-
-    </Navbar>
-  )
 }
 
 export default Header
