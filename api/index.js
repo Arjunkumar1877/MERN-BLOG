07d9d415581425afc7608aa4f19e9ae370e16500
Arjun kumar vs <arjun.tech177@gmail.com>
2024-03-22 04:10:46 +0530

    Added user update api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 dotenv.config()
 
 mongoose.connect(process.env.MONGO)
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO)
 })
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
