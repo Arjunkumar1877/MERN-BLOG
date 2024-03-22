fc27010dbeba4c2dc06248db6068ce57edccc480
Arjun kumar vs <arjun.tech177@gmail.com>
2024-03-23 03:08:14 +0530

    Addded admin delete users functionality. and complete  post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
+import PostPage from './pages/PostPage'
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
       <Route path='/edit-post/:postId' element={<UpdatePost />} />
     </Route>
     <Route path='/projects' element={<Projects />} />
+    <Route path='/post/:postSlug' element={<PostPage />} />
    </Routes>
 
    <Footer />
