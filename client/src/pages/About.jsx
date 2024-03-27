35b0d46efca57c2dafd5545b0ba03a0002c9d9c5
Arjun kumar vs <arjun.tech177@gmail.com>
2024-03-28 01:14:12 +0530

    completed about and projects pages..

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center ">
+      <div className="max-w-2xl mx-auto p-3">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">About Arjun's blog</h1>
+          <div className="flex flex-col text-md text-gray-500 gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore autem modi veritatis quia assumenda accusantium deleniti atque delectus deserunt, ea veniam nobis dicta vitae molestiae.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error distinctio dignissimos perferendis illo quas autem laudantium libero dolorem quia. Velit iure delectus accusamus aspernatur deleniti qui quas illo ex ea maxime nemo explicabo vero quam pariatur, consectetur at nihil quasi consequatur fugiat, earum autem corporis! Aliquam numquam fuga dolore esse.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo corporis adipisci quia dicta repudiandae ipsum!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
